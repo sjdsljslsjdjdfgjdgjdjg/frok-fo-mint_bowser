5de2f7ea9238c905b527b41ecc0dad1d7195d7c2
sjdsljslsjdjdfgjdgjdjg <childsjamesmartin@gmail.com>
2026-04-21 01:11:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { fetchPage } from "./fetcher.js";
 import { sanitizeHTML } from "./sanitizer.js";
-import { renderPage } from "./renderer.js";
+import { renderPage } from "./app.js";
 
 const input = document.getElementById("urlInput");
 const button = document.getElementById("goBtn");
